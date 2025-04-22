08ae037fd30282b13d4efe53a988e79024c7e4dd
JJAMJJAM2 <qhshapxls12@gmail.com>
2025-04-22 23:16:04 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,8 @@
+#!/bin/sh
 
-#!/bin/bash
+export N8N_BASIC_AUTH_ACTIVE=true
+export N8N_BASIC_AUTH_USER="jjamjjam2"
+export N8N_BASIC_AUTH_PASSWORD="Dntkd0810!"
+export N8N_USER_MANAGEMENT_DISABLED=true
+
 n8n
