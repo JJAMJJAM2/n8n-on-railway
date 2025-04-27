dea673244b094bfb97f7b961f1d67de442d5fc76
JJAMJJAM2 <qhshapxls12@gmail.com>
2025-04-27 21:59:38 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
-#!/bin/sh
-set -e
-
-# 기본 인증 정보는 Railway Variables 에서 설정했다고 가정
-# (N8N_BASIC_AUTH_ACTIVE, _USER, _PASSWORD)
-
-exec n8n start \
-  --tunnel \
-  --host "0.0.0.0" \
-  --port "$PORT"
+- exec n8n start \
+-   --tunnel \
+-   --host "${N8N_HOST:-0.0.0.0}" \
+-   --port "${N8N_PORT:-5678}"
++ exec n8n start \
++   --tunnel \
++   --host 0.0.0.0 \
++   --port "$PORT"
