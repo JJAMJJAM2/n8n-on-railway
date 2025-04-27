066093a50850cbeed4411a2950c4fb3d15a167e5
JJAMJJAM2 <qhshapxls12@gmail.com>
2025-04-27 21:28:32 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
+set -e
 
-export N8N_BASIC_AUTH_ACTIVE=true
-export N8N_BASIC_AUTH_USER="jjamjjam2"
-export N8N_BASIC_AUTH_PASSWORD="Dntkd0810!"
-export N8N_USER_MANAGEMENT_DISABLED=true
-
-n8n
+# n8n 을 Tunnel 모드로, 호스트·포트를 환경 변수로 받아 실행
+exec n8n start \
+  --tunnel \
+  --host "${N8N_HOST:-0.0.0.0}" \
+  --port "${N8N_PORT:-5678}"
