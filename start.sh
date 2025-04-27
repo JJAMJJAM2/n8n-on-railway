ff3e942a0d8d35dd6cb10694379132627bf2298a
JJAMJJAM2 <qhshapxls12@gmail.com>
2025-04-27 21:43:05 +0900

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 set -e
 
-# n8n 을 Tunnel 모드로, 호스트·포트를 환경 변수로 받아 실행
+# 기본 인증 정보는 Railway Variables 에서 설정했다고 가정
+# (N8N_BASIC_AUTH_ACTIVE, _USER, _PASSWORD)
+
 exec n8n start \
   --tunnel \
-  --host "${N8N_HOST:-0.0.0.0}" \
-  --port "${N8N_PORT:-5678}"
+  --host "0.0.0.0" \
+  --port "$PORT"
